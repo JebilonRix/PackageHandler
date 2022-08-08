49720a7e263a34b6398ab6c9a93b417677e31247
Fuat Can ERYİĞİT <fceryigit03@gmail.com>
2022-08-08 20:30:33 +0300

    file path fixing

--- Editor/Core/JsonHelper.cs
+++ Editor/Core/JsonHelper.cs
@@ -8,6 +8,8 @@ namespace RedPanda.PackageHandler
 {
     public static class JsonHelper
     {
+        private const string directoryName = "Unity Package Handler";
+
         public static void SaveToJson<T>(List<T> listToSave, string filename)
         {
             string content = ToJson<T>(listToSave.ToArray(), true);
@@ -57,11 +59,20 @@ namespace RedPanda.PackageHandler
         }
         private static string GetPath(string fileName)
         {
-            return "../Library/PackageCache/com.redpanda.package-handler.editor@0b8ae695fb" + "/" + fileName;
-            //return Application.dataPath + "/" + fileName;
+            //Check if the file exists in my documents folder.
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/" + directoryName;
+
+            //if file doesn't exist in my documents folder, creates directory.
+            if (!File.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            return path + "/" + fileName;
         }
         private static string ReadFile(string path)
         {
+            //Reads the file.
             if (!File.Exists(path))
             {
                 return "";
@@ -69,14 +80,16 @@ namespace RedPanda.PackageHandler
 
             using StreamReader reader = new(path);
             string content = reader.ReadToEnd();
+            reader.Close();
             return content;
         }
         private static void WriteFile(string path, string content)
         {
+            //Writes to the file.
             FileStream fileStream = new(path, FileMode.Create);
-
             using StreamWriter writer = new(fileStream);
             writer.Write(content);
+            writer.Close();
         }
     }
 }
