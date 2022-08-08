4291ebd7ec39bf2059cd10ddaa7e157a1f6b15c4
Fuat Can ERYİĞİT <fceryigit03@gmail.com>
2022-08-08 21:55:02 +0300

    test

--- Editor/Core/InstallPackage.cs
+++ Editor/Core/InstallPackage.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using UnityEditor.PackageManager;
 using UnityEditor.PackageManager.Requests;
@@ -33,7 +35,7 @@ namespace RedPanda.PackageHandler
                 await Task.Delay(100);
             }
 
-            Debug.Log($"{packageName} is loaded.");
+            //Debug.Log($"{packageName} is loaded.");
         }
         private static void AddGitPackages(string[] companyName, string[] packageName, string[] url)
         {
@@ -45,26 +47,87 @@ namespace RedPanda.PackageHandler
                 return;
             }
 
-            string keyword = "\"dependencies\": {";
+            //Creates lists from arrays.
+            List<string> localCompanyName = companyName.ToList();
+            List<string> localPackageName = packageName.ToList();
+            List<string> localUrl = url.ToList();
+
+            //Creates lists from manifests.
+            List<string> companyNamesFromManifest = new();
+            List<string> packageNamesFromManifest = new();
+
+            char dot = '.';
+            char quotes = '"';
+
+            //Reads all lines from manifest.
+            string[] allLines = File.ReadAllLines(path);
+
+            for (int i = 2; i < allLines.Length - 2; i++)
+            {
+                allLines[i] = allLines[i].Trim();
+                string[] texts = allLines[i].Split(new char[] { dot, quotes });
+
+                //Debug.Log(texts[2]);
+
+                if (texts[2] != "unity")
+                {
+                    companyNamesFromManifest.Add(texts[2]);
+                    packageNamesFromManifest.Add(texts[3]);
+
+                    //Debug.Log(texts[2] + "." + texts[3]);
+                }
+            }
+
+            for (int i = 0; i < localPackageName.Count; i++)
+            {
+                for (int j = 0; j < packageNamesFromManifest.Count; j++)
+                {
+                    if (packageNamesFromManifest[j] == localPackageName[i])
+                    {
+                        localCompanyName.RemoveAt(i);
+                        localPackageName.RemoveAt(i);
+                        localUrl.RemoveAt(i);
+                    }
+                }
+            }
+
+            if (localPackageName.Count == 0)
+            {
+                return;
+            }
+
+            //Prepares lines
             string lines = "";
 
-            for (int i = 0; i < packageName.Length; i++)
+            for (int i = 0; i < localPackageName.Count; i++)
             {
-                string name = Name(companyName[i], packageName[i]);
+                string name = Name(localCompanyName[i], localPackageName[i]);
 
-                lines += $"\t\t\"{name}\" : " + $"\"{url[i]}\",\n";
+                if (i == localPackageName.Count - 1)
+                {
+                    lines += $"\t\t\"{name}\" : " + $"\"{localUrl[i]}\",";
+                }
+                else
+                {
+                    lines += $"\t\t\"{name}\" : " + $"\"{localUrl[i]}\",\n";
+                }
             }
+
+            //Reads the file and adds lines to manifest
+            string keyword = "\"dependencies\": {";
 
             using StreamReader sr = new(path);
             string text = sr.ReadToEnd();
             text = text.Replace(keyword, keyword + "\n" + lines);
+            text.Trim();
             sr.Close();
 
+            //Writes to the file.
             using StreamWriter sw = new(path);
             sw.Write(text);
             sw.Close();
 
-            Debug.Log("Packages are added to project from git.");
+            //Debug.Log("Packages are added to project from git.");
 
             Client.Resolve();
         }
